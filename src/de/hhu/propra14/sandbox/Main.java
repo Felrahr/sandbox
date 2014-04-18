3f0a5a959af75166f441579310b71f8699934188
Sebastian Georg <Sebastian.Georg@hhu.de>
2014-04-18 12:46:42 +0200

    Try another change

--- src/de/hhu/propra14/sandbox/Main.java
+++ src/de/hhu/propra14/sandbox/Main.java
@@ -3,8 +3,8 @@ package de.hhu.propra14.sandbox;
 public class Main { 
 	public static void main(String[] args) {
         for( int i=0;i<4;i++)
-            if(i==5)
-                System.out.println("something is wrong!");
+            if(i==3)
+                System.out.println("something is right!");
 
     }
 }
