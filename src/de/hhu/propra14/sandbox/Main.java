eb9f4c928f3b6deb1d5e9f2571323623a1f8c0b3
Sebastian Georg <Sebastian.Georg@hhu.de>
2014-04-18 12:40:06 +0200

    Add random loop in Main class

--- src/de/hhu/propra14/sandbox/Main.java
+++ src/de/hhu/propra14/sandbox/Main.java
@@ -1,7 +1,10 @@
 package de.hhu.propra14.sandbox; 
-!
+
 public class Main { 
-	public static void main(String[] args) { 
-	
-	} 
+	public static void main(String[] args) {
+        for( int i=0;i<4;i++)
+            if(i==5)
+                System.out.println("something is wrong!");
+
+    }
 }
