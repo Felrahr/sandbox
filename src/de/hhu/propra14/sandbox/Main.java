316b325b71c3aebc3bb232a4deda435de0715982
Rafael Ehren <Rafael.Ehren@uni-duesseldorf.de>
2014-04-19 01:32:19 +0200

    changed main

--- src/de/hhu/propra14/sandbox/Main.java
+++ src/de/hhu/propra14/sandbox/Main.java
@@ -1,10 +1,13 @@
 package de.hhu.propra14.sandbox; 
 
-public class Main { 
-	public static void main(String[] args) {
-        for( int i=0;i<4;i++)
-            if(i!=5)
-                System.out.println("something is right!");
-
+public class Main {
+    public static void main(String[] args) {
+        for (int i = 0; i < 10; i++) {
+            if (i==5)i++;
+            if (i < 5)
+                System.out.println("smaller than five!" + " " + i);
+            else System.out.println("bigger than five!" + " " + i);
+        }
     }
+
 }
